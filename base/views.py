35fb7322eb4f8d09061973d9b046ad14e046c09d
AdnanK1 <adnangitonga@gmail.com>
2022-06-04 21:41:09 +0300

    Views:added views for login and register pages

--- base/views.py
+++ base/views.py
@@ -1,6 +1,15 @@
+from operator import concat
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
     context = {}
     return render(request,'home.html',context)
+
+def login(request):
+    context = {}
+    return render(request,'login.html',context)
+
+def register(request):
+    context = {}
+    return render(request,'register.html',context)
