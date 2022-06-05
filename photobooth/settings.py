a67fa553222ebf3f6f53856db71806658a4de0d8
AdnanK1 <adnangitonga@gmail.com>
2022-06-05 15:28:34 +0300

    Templates:Added crispy form to register form

--- photobooth/settings.py
+++ photobooth/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'base',
     'cloudinary',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -138,3 +139,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
