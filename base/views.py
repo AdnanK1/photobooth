e6b514a2ca8e1e4e4802036531d6909d20a65480
AdnanK1 <adnangitonga@gmail.com>
2022-06-05 18:09:31 +0300

    Base:Added the search functionality

--- base/views.py
+++ base/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.db.models import Q
 from .forms import CreateUserForm
+from .models import Image
+
 # Create your views here.
 def home(request):
-    context = {}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    images = Image.objects.filter(
+        Q(name__icontains=q) |
+        Q(caption__icontains=q) 
+    )
+    context = {'images':images}
     return render(request,'home.html',context)
 
 def register(request):
