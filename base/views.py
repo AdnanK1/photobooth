637d5bde6e343c3ea14e7e8d4dab8a9841843abb
AdnanK1 <adnangitonga@gmail.com>
2022-06-05 14:43:57 +0300

    Base:Made the registration form and implemented the save function in views.py file

--- base/views.py
+++ base/views.py
@@ -1,15 +1,21 @@
 from operator import concat
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
     context = {}
     return render(request,'home.html',context)
 
+def register(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form':form}
+    return render(request,'register.html',context)
+
 def login(request):
     context = {}
     return render(request,'login.html',context)
-
-def register(request):
-    context = {}
-    return render(request,'register.html',context)
